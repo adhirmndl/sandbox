fe2b3c4d5eeee6660e7419b7f637588a7c3a2e10
sayedasadali <sayedasadali@gmail.com>
2014-08-13 13:59:18 -0700

    [compartmentalized]

---
+++ hackerrank/GemStones.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class GemStones {
+
+ public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int _a;
+        _a = in.nextInt();
+        String firstWord = in.nextString(); 
+        System.out.println(firstWord)
+     }        
+   }
+}
