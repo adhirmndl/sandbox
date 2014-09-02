8647905871a23a7ae14e6178a85b314ff75cbd0d
sayedasadali <sayedasadali@gmail.com>
2014-09-01 23:56:29 -0700

    counting insertion

--- hackerrank/countingsort1.py
+++ hackerrank/countingsort1.py
@@ -3,8 +3,19 @@ def countingsort(a):
 	ans = [0]*100
 	for e in a:
 		ans[e] +=1
+	i = 0
+	output = '' 
+	for n in ans:
+		output += (str(i)+ ' ')*n	
+		i+=1
+	sumarr = 0
+	for i in range(len(ans)):
+		sumarr += ans[i]
+		ans[i] = sumarr
 	print ' '.join(map(str,ans))
 
-b = input()
-inputArr = [int(i) for i in raw_input().strip().split()]
+n = input()
+inputArr = []
+for i in range(n):
+	inputArr.append(int(raw_input().strip().split()[0]))
 countingsort(inputArr)
