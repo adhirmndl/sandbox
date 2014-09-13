3e6ae07296a77bf0cc720667ebea7efad94522e5
sayedasadali <sayedasadali@gmail.com>
2014-09-13 14:37:21 -0700

    hanoi_ds

--- algos/logoturtle.py
+++ algos/logoturtle.py
@@ -27,7 +27,7 @@ def otree(branchLen,t):
         t.backward(branchLen)
 import random
 def tree(length, t):
-    col = ["red", "orange", "blue", "yellow", "white", "gray", "green", "magenta", "pink"]
+    col = ['blue','red','green','white','yellow','violet','orange']
     if length > 5:
         t.color(col[random.randrange(0, len(col))])
         t.right(20)
@@ -40,7 +40,7 @@ def tree(length, t):
         t.backward(length)
         t.right(20)
 
-def main():
+def main_tree():
     t = turtle.Turtle()
     myWin = turtle.Screen()
     t.left(90)
@@ -50,5 +50,44 @@ def main():
     t.color("green")
     tree(50,t)
     myWin.exitonclick()
+
+def drawTriangle(points, color, myT):
+    myT.fillcolor(color)
+    myT.up()
+    myT.goto(points[0][0], points[0][1])
+    myT.down()
+    myT.begin_fill()
+    myT.goto(points[1][0], points[1][1])
+    myT.goto(points[2][0], points[2][1])
+    myT.goto(points[0][0], points[0][1])
+    myT.end_fill()
+
+def getMid(p1, p2):
+    return (p1[0] + p2[0])/2 , (p1[1] + p2[1])/2
+
+def sierpinski(points,degree,myTurtle):
+    colormap = ['blue','red','green','white','yellow',
+                'violet','orange']
+    drawTriangle(points,colormap[degree],myTurtle)
+    if degree > 0:
+        sierpinski([points[0],
+                        getMid(points[0], points[1]),
+                        getMid(points[0], points[2])],
+                   degree-1, myTurtle)
+        sierpinski([points[1],
+                        getMid(points[0], points[1]),
+                        getMid(points[1], points[2])],
+                   degree-1, myTurtle)
+        sierpinski([points[2],
+                        getMid(points[2], points[1]),
+                        getMid(points[0], points[2])],
+                   degree-1, myTurtle)
+
+def main():
+   myTurtle = turtle.Turtle()
+   myWin = turtle.Screen()
+   myPoints = [[-100,-50],[0,100],[100,-50]]
+   sierpinski(myPoints,3,myTurtle)
+   myWin.exitonclick()
 
 main()
