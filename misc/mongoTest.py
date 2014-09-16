87664ca4e35d75b17ba677ca7710282673fde7d3
sayedasadali <sayedasadali@gmail.com>
2014-09-16 13:57:46 -0700

    backup

--- misc/mongoTest.py
+++ misc/mongoTest.py
@@ -1,1 +1,15 @@
-#mongod.py
+from pymongo import MongoClient
+
+client = MongoClient('10.16.65.182:27017')
+
+db = client['test']
+
+collection = db['testData']
+print collection.insert({	'author' : 'ABCD',
+							'text' : 'acdadf'
+						})
+
+
+print db.collection_names()
+
+print db.find_one()
